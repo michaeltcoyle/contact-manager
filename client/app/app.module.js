afead8ed2776c22acc826e2f3c4376c060f590d2
Michael Coyle <michaeltcoyle@gmail.com>
2018-05-08 02:07:22 -0400

    added @angular/animations dependency

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -7,6 +7,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var platform_browser_1 = require("@angular/platform-browser/src/platform-browser");
+var animations_1 = require("@angular/platform-browser/animations");
 var core_1 = require("@angular/core/src/core");
 var app_component_1 = require("./app.component");
 var AppModule = /** @class */ (function () {
@@ -14,7 +15,7 @@ var AppModule = /** @class */ (function () {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule],
+            imports: [platform_browser_1.BrowserModule, animations_1.BrowserAnimationsModule],
             declarations: [app_component_1.AppComponent],
             bootstrap: [app_component_1.AppComponent]
         })
--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser/src/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core/src/core';
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './components/contacts/contacts.component'
 
 @NgModule({
-    imports:        [BrowserModule],
+    imports:        [BrowserModule, BrowserAnimationsModule],
     declarations:   [AppComponent],
     bootstrap:  [AppComponent]
 })
