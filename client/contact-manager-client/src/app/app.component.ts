45bb6ef21b6c6ffb724576680dd1b42d7e33d5ce
Michael Coyle <michaeltcoyle@gmail.com>
2018-05-11 18:24:01 -0400

    added remote data population and display to contact list view table with ag-grid

--- client/contact-manager-client/src/app/app.component.ts
+++ client/contact-manager-client/src/app/app.component.ts
@@ -2,42 +2,62 @@ import { Component, OnInit } from '@angular/core';
 import { ContactsService } from './contacts.service';
 import { Contact } from './contact.model';
 import { HttpClient } from '@angular/common/http';
+import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
 
+
+//Declare custom type for table Rows
+type agRow = {name: string, phone: string, email: string};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent implements OnInit {
 
-    //Create Table Headers
-    title = 'app';
+  //Create Table Headers
+  title = 'app';
 
-    columnDefs = [
-      {headerName: 'Name', field: 'name' },
-      {headerName: 'Phone', field: 'phone' },
-      {headerName: 'Email', field: 'email' }
-    ];
-    
-    rowData: any[] = new Array<any>();
-    //Get Contact Data
-    contacts: Contact[] = new Array();
-    constructor(private contactsService: ContactsService) {
-      this.contactsService.getContacts().subscribe(data => {
-      this.contacts = data;
-      });
-    }
-    
-    ngOnInit(){
-      this.contacts.forEach(((entry: Contact, i: number) => {
-          this.rowData[i] = new Array<any>();
-          this.rowData[i].name = entry.firstName.concat(" ", entry.lastName);
-          this.rowData[i].phone = entry.phone;
-          this.rowData[i].email = entry.email;
-          i = i + 1;
-      }), this);
-    }
+  columnDefs = [
+    {headerName: 'Name', field: 'name' },
+    {headerName: 'Phone', field: 'phone' },
+    {headerName: 'Email', field: 'email' }
+  ];
+  
+
+  //Initialize Fields
+  rowData: agRow[];
+  contacts: Contact[] = new Array();
+
+
+  //Get Contact Data
+  constructor(private contactsService: ContactsService) {
+    this.contactsService.getContacts().subscribe(data => {
+    this.contacts = data;
+    this.rowData = this.populateRows(this.contacts);
+    });
   }
+
+  //Populate Rows with Contacts
+  populateRows(data: Contact[]): agRow[] {
+    this.rowData = new Array<agRow>(data.length);
+    var index: number = 0;
+    var namefl: string;
+    for(let contact of data){
+      namefl = contact.firstName.concat(" ").concat(contact.lastName);
+      this.rowData[index] = {name: namefl, phone: contact.phone, email: contact.email};
+      index = index+1;
+    }
+    return this.rowData;
+  }
+
+  //Do on page initialization
+  ngOnInit(){
+
+  }
+}
   
 
