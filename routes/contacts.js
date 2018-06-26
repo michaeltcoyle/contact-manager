f03db7e510d624dec830a36a14085aa91328883a
Michael Coyle <michaeltcoyle@gmail.com>
2018-06-26 19:21:45 -0400

    added error check and message for not having db_login.json properly placed

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,100 +1,112 @@
+
+
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
 var cors = require('cors');
 var fs = require('fs');
-var data = fs.readFileSync('routes/db_login.json', 'utf8');
-var dblogin = JSON.parse(data);
+try{
+    var data = fs.readFileSync('routes/db_login.json', 'utf8');
+}
+catch (e){
+    console.log("Please add db_login.json to contact_manager/routes for database authorization.")
+    return process.exit(0);
+}
 
-//debug: print dblogin token to console
-//console.log(dblogin);
+    var dblogin = JSON.parse(data);
 
-var db = mongojs(dblogin.dblogin, ['contacts']);
 
-//get all contacts
-router.options('/contacts', cors());
-router.get('/contacts', function(req, res, next){
-    db.contacts.find(function(err, contacts){
-        if (err){
-            res.send(err);
-        }
-        res.json(contacts);
-    });
-});
 
-//get a single contact
-router.get('/contact/:id', function(req, res, next){
-    db.contacts.findOne({_id: mongojs.ObjectId(req.params.id)},function(err, contact){
-        if (err){
-            res.send(err);
-        }
-        res.json(contact);
-    });
-});
+    //debug: print dblogin token to console
+    //console.log(dblogin);
 
-module.exports = router;
+    var db = mongojs(dblogin.dblogin, ['contacts']);
 
-//save contact
-router.post('/contact', function(req, res, next){
-    var contact = req.body;
-    if (!contact.firstName || !contact.lastName){
-        res.status(400)
-        res.json({
-            "error": "Contact must have a name"
+    //get all contacts
+    router.options('/contacts', cors());
+    router.get('/contacts', function(req, res, next){
+        db.contacts.find(function(err, contacts){
+            if (err){
+                res.send(err);
+            }
+            res.json(contacts);
         });
-    }  else {
-        db.contacts.save(contact, function(err, contact){
+    });
+
+    //get a single contact
+    router.get('/contact/:id', function(req, res, next){
+        db.contacts.findOne({_id: mongojs.ObjectId(req.params.id)},function(err, contact){
             if (err){
                 res.send(err);
             }
             res.json(contact);
         });
-    }
-});
-
-//delete contact
-router.delete('/contact/:id', function(req, res, next){
-    db.contacts.remove({_id: mongojs.ObjectId(req.params.id)},function(err, contact){
-        if (err){
-            res.send(err);
-        }
-        res.json(contact);
     });
-});
 
-//edit contact
-router.put('/contact/:id', function(req, res, next){
-    var contact = req.body;
-    var updContact = {};
+    module.exports = router;
 
-    if (contact.firstName){
-        updContact.firstName = contact.firstName;
-    }
+    //save contact
+    router.post('/contact', function(req, res, next){
+        var contact = req.body;
+        if (!contact.firstName || !contact.lastName){
+            res.status(400)
+            res.json({
+                "error": "Contact must have a name"
+            });
+        }  else {
+            db.contacts.save(contact, function(err, contact){
+                if (err){
+                    res.send(err);
+                }
+                res.json(contact);
+            });
+        }
+    });
 
-    if (contact.lastName){
-        updContact.lastName = contact.lastName;
-    }
+    //delete contact
+    router.delete('/contact/:id', function(req, res, next){
+        db.contacts.remove({_id: mongojs.ObjectId(req.params.id)},function(err, contact){
+            if (err){
+                res.send(err);
+            }
+            res.json(contact);
+        });
+    });
 
-    if (contact.email){
-        updContact.email = contact.email;
-    }
+    //edit contact
+    router.put('/contact/:id', function(req, res, next){
+        var contact = req.body;
+        var updContact = {};
 
-    if (contact.phone){
-        updContact.phone = contact.phone;
-    }
+        if (contact.firstName){
+            updContact.firstName = contact.firstName;
+        }
+
+        if (contact.lastName){
+            updContact.lastName = contact.lastName;
+        }
+
+        if (contact.email){
+            updContact.email = contact.email;
+        }
+
+        if (contact.phone){
+            updContact.phone = contact.phone;
+        }
+        
+        if (!updContact){
+            res.status(400);
+            res.json({
+                "error":"Bad data; tried to edit empty contact"
+            });
+        } else {
+            db.contacts.update({_id: mongojs.ObjectId(req.params.id)}, updContact, {}, function(err, contact){
+                if (err){
+                    res.send(err);
+                }
+                res.json(contact);
+            });
+        }
     
-    if (!updContact){
-        res.status(400);
-        res.json({
-            "error":"Bad data; tried to edit empty contact"
-        });
-    } else {
-        db.contacts.update({_id: mongojs.ObjectId(req.params.id)}, updContact, {}, function(err, contact){
-            if (err){
-                res.send(err);
-            }
-            res.json(contact);
-        });
-    }
-});
+    });
 
